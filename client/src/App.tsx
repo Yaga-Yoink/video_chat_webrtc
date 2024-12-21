a2ae404cbc6a01d2ffeea514e7f2b57f10a09c5f
Yaga-Yoink <rfujii3012@gmail.com>
2024-12-20 23:12:00 -0700

    Added local proxy server and data fetching
    - Fetch data from backend api
    - Update the webpage with the data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("before fetching");
+  useEffect(() => {
+    console.log("fetching data");
+    fetch("/api")
+      .then((data) => data.json())
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.error("Issue getting data", error);
+      });
+  }, []);
 
   return (
     <>
@@ -25,11 +35,14 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div>
+        <p>{data.toString()}</p>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import path from "path";
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const messages = require("./messages.ts");
 
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.send(messages.home);
+  console.log('api was fetched')
+  res.send(["very interesting data"]);
 });
-
 
 app.use((req, res) => {
   res.status(404).send(messages.notFound);
