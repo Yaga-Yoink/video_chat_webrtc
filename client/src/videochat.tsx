197633be905d279994530bc8cf9b102be59e4bf8
Yaga-Yoink <rfujii3012@gmail.com>
2024-12-21 00:48:39 -0700

    Video captures after button is pressed now

--- client/src/videochat.tsx
+++ client/src/videochat.tsx
@@ -8,17 +8,19 @@ const VideoChat: FC<VideoChatProps> = ({ updateTrigger }) => {
   const constraints = { audio: true, video: true };
   const videoRef = useRef<HTMLVideoElement>(null);
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia(constraints).then((mediaStream) => {
-      if (videoRef.current) {
-        videoRef.current.srcObject = mediaStream;
-      }
-    });
-    return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        const stream = videoRef.current.srcObject as MediaStream;
-        stream.getTracks().forEach((track) => track.stop);
-      }
-    };
+    if (updateTrigger != 0) {
+      navigator.mediaDevices.getUserMedia(constraints).then((mediaStream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = mediaStream;
+        }
+      });
+      return () => {
+        if (videoRef.current && videoRef.current.srcObject) {
+          const stream = videoRef.current.srcObject as MediaStream;
+          stream.getTracks().forEach((track) => track.stop);
+        }
+      };
+    }
   }, [updateTrigger]);
   return (
     <div>
